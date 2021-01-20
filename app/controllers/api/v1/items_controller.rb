e519a2a1f29fef1a4a1b9cfdbd95f9d42b915b1a
daniellecroxton <danikrist@gmail.com>
2021-01-20 15:50:53 -0700

    Updating controllers

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,34 @@
 class Api::V1::ItemsController < ApplicationController
+
+    def index
+        @items = Item.all
+        render json: @items
+    end
+
+    def create
+        @item = Item.build(item_params)
+            if @item.save
+                render json: @item
+            else
+                render json: {error: 'Item could not be saved.'}
+            end
+    end
+
+    def show
+        @item = Item.find(params[:id])
+        render json: @item
+    end
+
+    def destroy
+        @item = Item.find(params[:id])
+        @item.destroy
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:name, :description, :claimed_by, :picture, :price, :user_id)
+    end
+
 end
+
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::UsersController < ApplicationController
+
+
+    def create
+        @user = User.build(user_params)
+            if @user.save
+                render json: @user
+            else
+                render json: {error: 'User could not be saved.'}
+            end
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name)
+    end
+
 end
