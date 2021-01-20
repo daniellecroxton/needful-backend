e8c0594372cfbdc1069d106c95125cdfd6858cfd
daniellecroxton <danikrist@gmail.com>
2021-01-20 14:37:42 -0700

    added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+  user1 = User.create(name: 'johndoe85')
+  user2 = User.create(name: 'sallys123')
+  item1 = Item.create(name: '8 qt. Stockpot', description: 'Stainless steel 8 qt. stockpot with lid', price: 5, user_id:1 )
+  item2 = Item.create(name: 'Queen Mattress', description: 'Queen foam mattress, 2 years old', price: 100, user_id:2 )
+  item3 = Item.create(name: 'Down Jacket', description: 'Womens large down jacket in black', price: 20, user_id:2 )
+  item4 = Item.create(name: 'Washer', description: 'Top loading white washer, 5 cycles, good condition', price: 200, user_id:1 )
+  item5 = Item.create(name: 'Leather Boots', description: 'Mens brown lace-up leather boots, size 9', price: 15, user_id:1 )
+
