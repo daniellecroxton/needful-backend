6d79a08cacccbf882fb948f80523376f0f540cf0
daniellecroxton <danikrist@gmail.com>
2021-01-20 15:14:03 -0700

    added claim_item method to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
     belongs_to :user
     validates :name, :price, presence: true
+
+    def claim_item(user_id)
+        if claimed_by == nil
+            self.claimed_by = user_id
+            self.save
+        else
+            return 'Item has already been claimed.'
+        end
+    end
+
+
 end
