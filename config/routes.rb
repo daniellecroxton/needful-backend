f7de6e1b95479f68fe156d44181f093e222addcf
daniellecroxton <danikrist@gmail.com>
2021-01-20 11:32:38 -0700

    added api/v1 namespacing to routes.rb and controller files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :items
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+
+      resources :items
+      resources :users
+
+    end
+  end
+
 end
