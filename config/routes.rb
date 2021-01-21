e2b72789c51a765d86fbfa3abda70dd7a9c7c6dd
daniellecroxton <danikrist@gmail.com>
2021-01-21 14:43:52 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
+  root 'api/v1/items#index'
+
   namespace :api do
     namespace :v1 do
 
-      resources :items
-      resources :users
+      resources :items, except: :destroy
+
+      resources :users, only: [:index, :show, :create] do 
+        resources :items, only: :index, controller: 'user_items'
+      end
 
     end
   end
