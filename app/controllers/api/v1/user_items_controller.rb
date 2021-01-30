08a407498c6cd3f755bbbb164d6b4b8402b4db60
daniellecroxton <danikrist@gmail.com>
2021-01-29 17:31:20 -0700

    removed duplicated line of code in user items controller

--- app/controllers/api/v1/user_items_controller.rb
+++ app/controllers/api/v1/user_items_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::UserItemsController < ApplicationController
     before_action :set_user
 
     def index
-        @user = User.find(params[:user_id])
         @items = @user.items
         render json: @items
     end
